edc123d5ec769998f409e118a3e78ce337a49277
Masahiro Sakai <masahiro.sakai@gmail.com>
2018-02-16 15:01:01 +0900

    add test.py

---
+++ test.py
@@ -0,0 +1,19 @@
+import pyglucose
+
+solver = pyglucose.Solver()
+solver.set_incremental_mode()
+p = pyglucose.Lit(solver.new_var())
+q = pyglucose.Lit(solver.new_var())
+solver.add_clause([p,q])
+solver.add_clause([p,~q])
+solver.add_clause([~p,~q])
+solver.solve()
+assert solver.okay()
+m = solver.model()
+pv = m[p.var()]
+qv = m[q.var()]
+assert (pv or qv) and (pv or not qv) and (not pv or not qv)
+
+solver.add_clause([~p,q])
+solver.solve()
+assert not solver.okay()
